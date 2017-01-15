791ce0f341ba83d332711d33d619d2bfc4c1e19e
alv <alwinlazar46@gmail.com>
2017-01-15 11:48:25 +0530

    Initial Commit

---
+++ VidyaVox/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VidyaVox
+//
+//  Created by Alwin Lazar on 15/01/17.
+//  Copyright © 2017 Xeoscript Technologies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
